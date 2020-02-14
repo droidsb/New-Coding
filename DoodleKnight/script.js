ad98bd4d93196b5a74d24f650940b186983f8e5b
Seth Baird <droidsb@users.noreply.github.com>
2020-02-13 22:33:40 -0700

    Added DoodleKnight

---
+++ DoodleKnight/script.js
@@ -0,0 +1,81 @@
+var Engine = Matter.Engine,
+    //Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var firstbox;
+var world;
+
+var ground;
+
+var boxes=[];
+
+function setup() {
+
+	
+
+  createCanvas(1000, 800);
+  
+  
+  
+// create an engine
+engine = Engine.create();
+world=engine.world;
+
+
+// create two boxes and a ground
+
+
+//ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+boxes.push(new Box(400, 610, 410,60, {friction: 0.3,restitution: 0.6, isStatic: true, angle:radians(35)}));
+
+// add all of the bodies to the world
+//World.add(world, ground);
+
+// run the engine
+//Engine.run(engine); 
+
+
+  
+  
+}
+
+function mouseDragged(){
+
+	boxes.push(new Box(mouseX, mouseY, 20,20, {friction: 0.3,restitution: 0.6}));
+	
+	
+
+}
+
+function draw() {
+  background(150);
+  
+  noStroke();
+ Engine.update(engine);
+ 
+ for(var i=0; i<boxes.length; i++){
+ 
+ 	boxes[i].show();
+ 	
+ 	if(boxes[i].isOffScreen()){
+ 			
+ 			
+ 		boxes[i].removeFromWorld();
+ 			
+ 		//World.remove(world, boxes[i])
+ 	
+ 		boxes.splice(i,1);
+ 		
+ 		i--;
+ 	
+ 	}
+ 
+ }
+  //console.log(world.bodies.length);
+  
+  
+  
+}
