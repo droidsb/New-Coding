79bd241f8000902106100308d99e5f59c1676229
Seth Baird <droidsb@users.noreply.github.com>
2020-02-22 21:39:01 -0700

    Added Arrows
    There are still some issues with arrows sticking to their targets

--- DoodleKnight/box.js
+++ DoodleKnight/box.js
@@ -4,6 +4,8 @@ function Box(x, y, w, h, options) {
   this.h = h;
   World.add(world, this.body);
   
+ 
+  
   this.isOffScreen =function(){
 	 var pos=this.body.position;
 	 
@@ -16,6 +18,34 @@ function Box(x, y, w, h, options) {
   	World.remove(world,this.body);
   
   }
+  
+  this.position = function() {
+  
+  
+  
+  	return this.body.position;
+  
+  }
+  
+  this.angle = function() {
+  
+  
+  
+  	return this.body.angle;
+  
+  }
+  
+  this.type = function() {
+  
+  	return this.body.label;
+  
+  }
+  
+  this.id=function(){
+  
+  	return this.body.id;
+  
+  }
 
   this.show = function() {
     var pos = this.body.position;
@@ -24,10 +54,82 @@ function Box(x, y, w, h, options) {
     translate(pos.x+cam.x, pos.y+cam.y);
     rotate(angle);
     rectMode(CENTER);
-    strokeWeight(1);
-    stroke(255);
-    fill(127);
-    rect(0, 0, this.w, this.h);
+    
+    
+    
+if(this.body.label==="Crate"){
+    
+    fill(130, 91, 31);
+  strokeWeight(2);
+  stroke(80, 41, 0)
+  rect(0,0,this.w,this.h);
+  
+  
+  rotate(radians(45));
+  rect(0,0,this.w/10,sqrt(pow(this.h,2)+pow(this.w,2)))
+  
+  rotate(radians(90));
+  
+  rect(0,0,this.w/10,sqrt(pow(this.h,2)+pow(this.w,2)))
+  
+  }
+  
+  else{
+  	fill(127)
+  	stroke(255)
+  	rect(0,0,this.w,this.h);
+  }
+  
+  if(this.body.label==="Player"){
+  
+  	//console.log(this.body);
+  	if(abs(this.body.velocity.x)<8){
+  	this.body.force.x=player.xv;
+  	}
+  	
+  	if(abs(this.body.velocity.y)<8){
+  	this.body.force.y=player.yv;
+  	
+  	}
+  	//console.log(this.body.velocity.x)
+  	
+  
+  }
+  
+  
+  
+  if(this.body.label==="FlyingArrow"){
+  
+
+   
+   
+   
+   var maxspeed=0.2;
+  
+  if(this.body.velocity.x>maxspeed){
+  	Matter.Body.setAngle(this.body,-atan2(this.body.velocity.x,this.body.velocity.y))
+  	
+  	}
+  	
+  	
+  
+  }
+    for(var p=0; p<staticArrows.length; p++){
+    
+    if(this.body.id===staticArrows[p].id){
+    	
+    	rotate(-staticArrows[p].oldAngle)
+    	
+    	rect(-staticArrows[p].xOff,-staticArrows[p].yOff,40,5);
+    	
+    
+    }
+    
+    
+    }
+    
     pop();
+    
+   
   };
 }
--- DoodleKnight/script.js
+++ DoodleKnight/script.js
@@ -1,7 +1,9 @@
 var Engine = Matter.Engine,
     //Render = Matter.Render,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Events = Matter.Events,
+    Constraint= Matter.Constraint;
 
 var engine;
 var firstbox;
@@ -11,15 +13,45 @@ var ground;
 
 var boxes=[];
 
+var constraints=[];
+
 var cam={x:0, y:0, speed:2};
 
 var keys=[];
+
+var player={xv:0, yv:0, speed:0.005}
+
+var staticArrows=[];
+
+ var defaultCategory = 0x0001,
+        ArrowCategory = 0x0002,
+        PlayerCategory = 0x0004,
+        GroundCategory = 0x0008,
+        MobCategory = 0x0016,
+        NothingCategory=0x0032;
+        
+    
+        
+        var bowDraw = new Howl({
+ 				 src: ['Sound/BowDraw.mp3']
+        
+        });
+        
+        var arrowShoot = new Howl({
+ 				 src: ['Sound/ArrowShoot.mp3']
+        
+        });
+        
+        var arrowHit = new Howl({
+ 				 src: ['Sound/ArrowHit.mp3']
+        
+        });
 
 function setup() {
 
 	
 
-  createCanvas(1000, 800);
+  createCanvas(1200, 800);
   
   
   
@@ -27,13 +59,129 @@ function setup() {
 engine = Engine.create();
 world=engine.world;
 
+function collision(event){
+
+	var pairs=event.pairs;
+	
+	for(var i=0; i<pairs.length; i++){
+	
+	var bodyA=pairs[i].bodyA;
+	
+	var bodyB=pairs[i].bodyB;
+	
+	}
+	
+	//console.log(bodyB);
+	
+	//console.log(bodyB.label);
+	
+	//console.log(bodyA.label)
+
+	
+	if(bodyB.label==="FlyingArrow" && bodyA.label!=="FlyingArrow"){
+	
+		bodyB.label="StaticArrow"
+		
+		bodyB.collisionFilter.category=1;
+		
+		bodyB.collisionFilter.mask=NothingCategory;
+	
+	/*		var options1={
+
+	bodyA: bodyA,
+	bodyB, bodyB,
+	length: 0,
+	stiffness: 1,
+	pointA:{x:bodyB.position.x-bodyA.position.x, y:bodyB.position.y-bodyA.position.y},
+	pointB:{x:20,y:0}
+
+}
+
+constraints.push(new Cons(options1));
+
+var options2={
+
+	bodyA: bodyA,
+	bodyB, bodyB,
+	length: 0,
+	stiffness: 1,
+	pointA:{x:bodyB.position.x-bodyA.position.x, y:bodyB.position.y-bodyA.position.y},
+	pointB:{x:10,y:0}
+	
+	
+
+}
+
+constraints.push(new Cons(options2));
+
+
+*/
+
+staticArrows.push({id:bodyA.id, xOff: bodyA.position.x-bodyB.position.x, yOff: bodyA.position.y-bodyB.position.y, oldAngle: bodyA.angle ,l:40, h:5, angle: bodyB.angle})
+
+
+
+
+//var constraint1 = Constraint.create(options1);
+
+
+
+
+
+
+
+
+//console.log(bodyA.parts)
+
+		//bodyB.parent=bodyA;
+		
+		//bodyB.isStatic=true;
+		
+		arrowHit.play();
+		
+		
+		//console.log(bodyA)
+		
+		
+		
+		//Matter.Body.setParts(bodyA, [bodyA, bodyB])
+		
+		
+		
+		//Matter.Body.setParts(bodyA, [bodyA, bodyB])
+		
+		
+		
+		//bodyA.parts=[bodyA, Bodies.rectangle(0,0,5,40, {category:bodyA.category, mask:bodyA.mask})];
+			
+	}
+	
+}
+
+
+
+
+Events.on(engine, 'collisionStart', collision);
+
+
+
+
+
 
 // create two boxes and a ground
 
 
 //ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-boxes.push(new Box(400, 610, 410,60, {friction: 0.3,restitution: 0.6, isStatic: true, angle:radians(0)}));
+boxes.push(new Box(400, 610, 1000,60, {friction: 0.3,restitution: 0.6, isStatic: true, angle:radians(0), collisionFilter: {mask: GroundCategory | defaultCategory | ArrowCategory, category: GroundCategory}}));
+
+boxes.push(new Box(400, 550, 30,30, {friction: 0.3,restitution: 0.6, isStatic: false, angle:radians(0), label:"Player"}));
+
+
+boxes.push(new Box(550, 550, 30,50, {friction: 0.3,restitution: 0.6, isStatic: false, angle:radians(0), label:"Mob", collisionFilter :{mask: GroundCategory | defaultCategory | ArrowCategory | MobCategory, category: MobCategory}}));
+
+
+
 
 // add all of the bodies to the world
 //World.add(world, ground);
@@ -41,16 +189,98 @@ boxes.push(new Box(400, 610, 410,60, {friction: 0.3,restitution: 0.6, isStatic: 
 // run the engine
 //Engine.run(engine); 
 
+for(var i=0; i<20; i++){
 
+
+//boxes.push(new Box(400+i/10,0-i*10, 40,40, {friction: 0.3,restitution: 0.6}, "crate"));
+
+}
   
   
 }
 
+
+
 function mouseDragged(){
 
-	boxes.push(new Box(mouseX-cam.x, mouseY-cam.y, 20,20, {friction: 0.3,restitution: 0.6}));
 	
 	
+	
+
+}
+var id1;
+
+function mousePressed(){
+
+id1=bowDraw.play();
+
+
+
+}
+
+function mouseReleased(){
+
+bowDraw.fade(1, 0, 500, id1);
+
+arrowShoot.play();
+
+var boxpos={};
+
+for(var i=0; i<boxes.length; i++){
+
+	if(boxes[i].type()==="Player"){
+		
+		boxpos=boxes[i].position();
+	
+	}
+
+}
+
+
+
+
+var xd=mouseX-boxpos.x;
+
+var xy= mouseY-boxpos.y;
+
+
+
+
+//var angle=calcAngle(abs(xd), abs(xy));
+//console.log(degrees(angle))
+
+function angleBetween(p1,p2){
+
+
+return Math.atan2(p2.y-p1.y, p2.x-p1.x);
+
+}
+
+
+
+angle=angleBetween({x:mouseX, y:mouseY},{x:boxpos.x, y:boxpos.y});
+
+//angle=angle+radians(90);
+
+var l=sqrt(pow(xd,2)+pow(xy,2))
+
+var tv={x:-(l*cos(angle))/30000, y:-(l*sin(angle))/30000}
+
+/*if(xd<0){
+
+	tv.x=-tv.x;
+
+}
+
+if(xy>0){
+
+	tv.y=-tv.y;
+
+}*/
+
+//console.log(degrees(angle)-90)
+
+boxes.push(new Box(boxpos.x,boxpos.y, 5,40, {friction: 0.3, frictionAir: 0.01,restitution: 0.6, force: {x:tv.x, y:tv.y}, angle:radians(90), label:"FlyingArrow",collisionFilter: {mask: GroundCategory | ArrowCategory | MobCategory, category: ArrowCategory}}));
 
 }
 
@@ -58,11 +288,38 @@ function draw() {
   background(150);
   
   noStroke();
+  
+  
  Engine.update(engine);
+ rectMode(CENTER)
+ rect(200,200,10,10)
+ 
+		push();
+ 		
+ 		translate(200,180)
+ 		
+ 		rotate(radians(90))
+ 		
+ 		//translate(0,-20)
+ 		
+ 		//rectMode(CENTER);
+ 		rect(-40,0, 40,5)
+ 	
+ 	pop();
+ 	
+ 
+ 
  
  for(var i=0; i<boxes.length; i++){
  
  	boxes[i].show();
+ 	
+ 	
+
+ 	
+ 	
+ 	
+ 	
  	
  	if(boxes[i].isOffScreen()){
  			
@@ -78,9 +335,20 @@ function draw() {
  	}
  
  }
+ 
+ for(var i=0; i<constraints.length; i++){
+ 
+ 	constraints[i].show();
+ 
+ }
+ 
+ 
+ 
   //console.log(world.bodies.length);
   
-  text(cam.y,200,200);
+ // text(player.x,200,200);
+  
+  
   
   if(keys[38]){
   
@@ -105,6 +373,51 @@ function draw() {
   	cam.x=cam.x+cam.speed;
   
   }
+  
+  
+  
+  
+  if(keys[87]){
+  
+  	player.yv=-player.speed;
+  
+  }
+  
+  if(keys[83]){
+  
+  	player.yv=player.speed;
+  
+  }
+  
+ 
+  
+  if(keys[87]===false && keys[83]===false){
+
+	player.yv=0;
+
+}
+  
+  
+  
+  if(keys[65]){
+  
+  	player.xv=-player.speed;
+  
+  }
+  
+  
+  
+  if(keys[68]){
+  
+  	player.xv=player.speed;
+  
+  }
+  
+if(keys[68]===false && keys[65]===false){
+
+	player.xv=0;
+
+}
   
   
 }
